97a427409cbc5bcae1435233582ca20592c6a004
TCMSKhoa <158541300+KhoaAki@users.noreply.github.com>
2024-12-09 14:47:25 +0700

    Update models.py

--- models.py
+++ models.py
@@ -3,7 +3,8 @@ from database import Base
 import uuid
 from datetime import datetime
 from sqlalchemy.orm import relationship
-
+import pytz
+vn_tz = pytz.timezone('Asia/Ho_Chi_Minh')
 class Admin(Base):
     __tablename__ = "admin"
     admin_id = Column(String(36), primary_key=True)
