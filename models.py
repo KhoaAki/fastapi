4daf94c96ffc237023bbe31015fb6fd042e6e878
TCMSKhoa <158541300+KhoaAki@users.noreply.github.com>
2024-12-09 15:51:52 +0700

    Update models.py

--- models.py
+++ models.py
@@ -4,8 +4,6 @@ import uuid
 from datetime import datetime
 from sqlalchemy.orm import relationship
 from zoneinfo import ZoneInfo
-import pytz
-tz_VN = pytz.timezone('Asia/Ho_Chi_Minh')
 def get_vietnam_time():
     vietnam_tz = ZoneInfo("Asia/Ho_Chi_Minh")
     return datetime.now(tz=vietnam_tz)
@@ -137,4 +135,4 @@ class Feedback(Base):
     subject_id = Column(Integer, ForeignKey('subject.subject_id'), nullable=False)  # Bắt buộc phải có môn học
     is_parents = Column(Integer, default=0)  # 0 là feedback cha, 1 là feedback con
     parent_id = Column(String(36), nullable=True)  # Chỉ điền khi là feedback con
-    created_at = Column(DateTime, default=datetime.now(tz_VN))
+    created_at = Column(DateTime, default=get_vietnam_time)
