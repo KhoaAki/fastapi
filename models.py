2da0be86fecfaf271a673bd8ad1f8c7c6312779f
TCMSKhoa <158541300+KhoaAki@users.noreply.github.com>
2024-12-09 16:23:58 +0700

    Update models.py

--- models.py
+++ models.py
@@ -1,9 +1,8 @@
 from sqlalchemy import Column, String, Integer, DateTime, Boolean, ForeignKey, VARCHAR,Text, Float, func
 from database import Base
 import uuid
-from datetime import datetime
+from datetime import datetime, timedelta
 from sqlalchemy.orm import relationship
-
 class Admin(Base):
     __tablename__ = "admin"
     admin_id = Column(String(36), primary_key=True)
