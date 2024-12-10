fd7cc50f4db0e8b7bc3b14201bad91c164973ccc
TCMSKhoa <158541300+KhoaAki@users.noreply.github.com>
2024-12-10 17:06:20 +0700

    Update ResetPassModel.py

--- basemodel/ResetPassModel.py
+++ basemodel/ResetPassModel.py
@@ -1,5 +1,5 @@
 from typing import List, Optional
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 from datetime import datetime
 # Model Pydantic để đặt lại mật khẩu
 class ResetPasswordRequest(BaseModel):
